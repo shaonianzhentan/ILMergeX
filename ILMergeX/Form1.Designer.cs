65b37c4f2b5c033506601b45477c9e06d0796372
shaonianzhentan <shaonianzhentan@qq.com>
2016-07-28 15:36:43 +0800

    合并工具第一版

---
+++ ILMergeX/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ILMergeX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxFileName = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.删除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.清空ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbFrameWork = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxFileName
+            // 
+            this.listBoxFileName.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBoxFileName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxFileName.FormattingEnabled = true;
+            this.listBoxFileName.ItemHeight = 12;
+            this.listBoxFileName.Location = new System.Drawing.Point(0, 0);
+            this.listBoxFileName.Name = "listBoxFileName";
+            this.listBoxFileName.Size = new System.Drawing.Size(537, 253);
+            this.listBoxFileName.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.删除ToolStripMenuItem,
+            this.清空ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 48);
+            // 
+            // 删除ToolStripMenuItem
+            // 
+            this.删除ToolStripMenuItem.Name = "删除ToolStripMenuItem";
+            this.删除ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.删除ToolStripMenuItem.Text = "删除";
+            this.删除ToolStripMenuItem.Click += new System.EventHandler(this.删除ToolStripMenuItem_Click);
+            // 
+            // 清空ToolStripMenuItem
+            // 
+            this.清空ToolStripMenuItem.Name = "清空ToolStripMenuItem";
+            this.清空ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.清空ToolStripMenuItem.Text = "清空";
+            this.清空ToolStripMenuItem.Click += new System.EventHandler(this.清空ToolStripMenuItem_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMerge.Location = new System.Drawing.Point(450, 6);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(75, 32);
+            this.btnMerge.TabIndex = 7;
+            this.btnMerge.Text = "开始合并";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 6);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 32);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "添加合并文件";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cmbFrameWork);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.btnMerge);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 253);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(537, 47);
+            this.panel1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = ".net framework版本";
+            // 
+            // cmbFrameWork
+            // 
+            this.cmbFrameWork.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFrameWork.FormattingEnabled = true;
+            this.cmbFrameWork.Items.AddRange(new object[] {
+            "4.5",
+            "4.0",
+            "3.5",
+            "3.0",
+            "2.0"});
+            this.cmbFrameWork.Location = new System.Drawing.Point(231, 13);
+            this.cmbFrameWork.Name = "cmbFrameWork";
+            this.cmbFrameWork.Size = new System.Drawing.Size(121, 20);
+            this.cmbFrameWork.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBoxFileName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(537, 253);
+            this.panel2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 300);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DLL合并工具";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxFileName;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbFrameWork;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 删除ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 清空ToolStripMenuItem;
+    }
+}
+
---
+++ ILMergeX/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Resources;
+using System.Text;
+using System.Windows.Forms;
+using GoodHelper;
+using System.Diagnostics;
+
+namespace ILMergeX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosed += Form1_FormClosed;
+            cmbFrameWork.SelectedIndex = 1;
+        }
+
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (File.Exists("ILMerge.exe"))
+            {
+                File.Delete("ILMerge.exe");
+                File.Delete("ILMergeX.bat");                
+            }
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            string exeFile = "",exeFileName="",fileName;
+            List<string> list = new List<string>();
+            int i = 0,index=0;
+            foreach(string name in listBoxFileName.Items){
+                fileName = index + Path.GetExtension(name);
+                if (File.Exists(fileName)) {
+                    File.Delete(fileName);
+                }
+                index++;
+                if (Path.GetExtension(name).ToLower() == ".exe") {
+                    i++;
+                    exeFile = fileName;
+                    exeFileName=Path.GetFileName(name);
+                    continue;
+                }
+                list.Add(fileName);                
+            }
+            if (i != 1)
+            {
+                MessageBox.Show("只能有一个exe主程序，不能存在两个exe主程序！");
+            }
+            else
+            {
+                //将核心文件写入到本地
+                if (!File.Exists("ILMerge.exe"))
+                {
+
+                    System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+                    Stream fs = asm.GetManifestResourceStream("ILMergeX.Resources.ILMerge.exe");
+
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, (int)fs.Length);
+
+                    File.WriteAllBytes("ILMerge.exe", buffer);
+                }
+
+                //将所有要合并的程序复制到本地
+                index = 0;
+                foreach (string name in listBoxFileName.Items)
+                {
+                    fileName = index + Path.GetExtension(name);
+                    if (!File.Exists(fileName))
+                    {
+                        File.Copy(name, fileName);
+                    }
+                    index++;
+                }
+
+                //拼接所有DLL文件
+                string dll = "";
+                foreach (string dllname in list)
+                {
+                    dll += dllname + " ";
+                }
+
+                string cmd = "cd " + Application.StartupPath + " \n";
+                cmd += "ILMerge /ndebug /target:winexe /out:tmp.exe " + exeFile + " /log " + dll + " /targetplatform:"+cmbFrameWork.Text + " \n";
+                File.WriteAllText("ILMergeX.bat", cmd);
+                Process p = Process.Start("ILMergeX.bat");
+                while (p.WaitForExit(0) == false)
+                {
+
+                }
+
+                if (File.Exists("tmp.exe"))
+                {
+                    try
+                    {
+                        if (File.Exists(exeFileName))
+                        {
+                            exeFileName = "New-" + exeFileName;
+                            if (File.Exists(exeFileName)) File.Delete(exeFileName);
+                        }
+                        //当所有数据完事之后，删除数据
+                        index = 0;
+                        foreach (string name in listBoxFileName.Items)
+                        {
+                            File.Delete(index + Path.GetExtension(name));
+                            index++;
+                        }
+                        listBoxFileName.Items.Clear();
+
+                        FileInfo fi = new FileInfo("tmp.exe");
+                        fi.MoveTo(exeFileName);
+
+                        File.Delete("ILMerge.exe");
+                        File.Delete("ILMergeX.bat");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Filter = "exe文件和dll文件|*.exe;*.dll";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string name in fileDialog.FileNames) {
+                    listBoxFileName.Items.Add(name);
+                }
+            }
+        }
+
+        private void 删除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBoxFileName.SelectedIndex >= 0) {
+                listBoxFileName.Items.RemoveAt(listBoxFileName.SelectedIndex);
+            }
+        }
+
+        private void 清空ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBoxFileName.Items.Clear();
+        }
+        
+    }
+}
